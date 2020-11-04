6a936a31e25f3c37bd99b28488bc85ecb70780a0
xiawang1024 <xiawang1024@gmail.com>
2020-11-04 14:28:39 +0800

    add public

--- main.go
+++ main.go
@@ -7,12 +7,13 @@ import (
 	"github.com/kataras/iris/v12/middleware/recover"
 	"github.com/rs/cors"
 	"log"
+	"regexp"
 	"time"
 	"xorm.io/xorm"
 )
 
 type User struct {
-	Id int64
+	Id int64 `json:"id"`
 	Name string `form:"name" json:"name"`
 	Age int `form:"age" json:"age"`
 	Sex string `xorm:"varchar(10)" form:"sex" json:"sex"`
@@ -43,6 +44,20 @@ func main() {
 
 	app.UseRouter(logger.New())
 	app.UseRouter(recover.New())
+
+	app.HandleDir("/public",iris.Dir("./public"),iris.DirOptions{
+		IndexName: "index.html",
+		PushTargetsRegexp: map[string]*regexp.Regexp{
+			"/":iris.MatchCommonAssets,
+		},
+		Cache: iris.DirCacheOptions{
+			Enable: true,
+			Encodings: []string{"gzip"},
+			CompressIgnore: iris.MatchImagesAssets,
+			CompressMinSize: 30 * iris.B,
+		},
+	})
+
 	engine,err=xorm.NewEngine("mysql","root:password@tcp(localhost:3306)/iris?charset?utf8")
 
 	if err != nil {
@@ -59,12 +74,16 @@ func main() {
 		user.Get("/{name}", getUser)
 
 		user.Post("/", addUser)
+
+
 	}
 
-	err=app.Listen(":8080")
-	if err != nil {
-		log.Fatalf("app listening on port 8080 is failed: %v",err)
-	}
+	//err=app.Listen(":8080")
+	//if err != nil {
+	//	log.Fatalf("app listening on port 8080 is failed: %v",err)
+	//}
+
+	app.Run(iris.TLS(":443","mycert.crt","mykey.key"))
 }
 
 func addUser(ctx iris.Context) {
@@ -92,25 +111,43 @@ func addUser(ctx iris.Context) {
 }
 
 func getUser(ctx iris.Context)  {
+	//单条记录查询
+	//name:=ctx.Params().Get("name")
+	//user:=User{}
+	//has,err:=engine.Where("name=?",name).Desc("id").Get(&user)
+	//if err != nil {
+	//	ctx.StopWithError(iris.StatusBadRequest,err)
+	//	log.Fatalf("user Table find by name: %v",err)
+	//}
+	//if has {
+	//	ctx.JSON(iris.Map{
+	//		"code":0,
+	//		"data":user,
+	//	})
+	//}else {
+	//	ctx.JSON(iris.Map{
+	//		"code":0,
+	//		"message":"not found user",
+	//	})
+	//}
+
+
+	//多条记录查询
 	name:=ctx.Params().Get("name")
-	user:=User{}
-	has,err:=engine.Where("name=?",name).Desc("id").Get(&user)
+	users := make([]User,0)
+	err=engine.Where("name=?",name).Find(&users)
 	if err != nil {
 		ctx.StopWithError(iris.StatusBadRequest,err)
-		log.Fatalf("user Table find by name: %v",err)
-	}
-	if has {
-		ctx.JSON(iris.Map{
-			"code":0,
-			"data":user,
-		})
-	}else {
-		ctx.JSON(iris.Map{
-			"code":0,
-			"message":"not found user",
-		})
+		log.Fatalf("user find by name failed: %v",err)
 	}
 
+	ctx.JSON(iris.Map{
+		"code":0,
+		"data":users,
+	})
 }
+
+
+
 
 
